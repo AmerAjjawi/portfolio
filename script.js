4a06ceb766226d6975211db43dccd87a4434a010
Anthony Ajjawi <ajjawi_amer@hotmail.com>
2025-03-30 17:17:02 -0400

    added fetch statement

--- script.js
+++ script.js
@@ -1,42 +1,25 @@
-//button variable
-const btn = document.getElementById("btn1");
-// input variable 
-const input = document.getElementById("input-form").value;
-//form variable
-const form = document.getElementById("form-data");
 
-const handleFormSubmit = (event) => {
-    event.preventDefault();
-    //function to get data
-    const data = formToJSON(form.elements);
-   
 
+
+function postData() {
+    //button variable
+    const btn = document.getElementById("btn1");
+    // input variable 
+    const input = document.getElementById("input-form").value;
+    //form variable
+    const form = document.getElementById("form-data");
+
+    const data = new FormData();
+    
+    try {
+        const res = fetch('ajjawi_amer@hotmail.com.php', {
+            method: 'POST',
+             
+            body: data,
+        });
+
+        if (!res.ok) console.log(`POST failed with ${res.status}`);
+    } catch (err) {
+        console.log(err);
+    }
 }
-
-form.addEventListener("submit", handleFormSubmit);
-
-//extract the values
-//retrieve data
-let elements;
-
-const formToJSON = (elements) =>
-    [].reduce.call(
-      elements,
-      (data, element) => {
-        data[element.name] = element.value;
-        return data;
-      },
-      {}
-    );
-
-// btn.addEventListener("submit", () => {
-//     e.preventDefault();
-
-
-   
-
-
-//     // window.open('mailto:ajjawi_amer@hotmail.com?subject=subject&body=${input.value}');
-//     // const formToJson = elements = [].reduce.call(elements, (data, element))
-
-// })
